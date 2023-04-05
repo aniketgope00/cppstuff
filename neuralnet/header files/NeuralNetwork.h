1dc58336633e0597ea63155be2435ad2c34cea1b
aniketgope00 <aniketgope00@gmail.com>
2023-04-05 12:52:24 +0530

    neural net project

---
+++ neuralnet/header files/NeuralNetwork.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Matrix.h"
+#include <cstdlib>
+
+/*
+     1  2                   n
+[i/p]->[]  [] ....  
+       [] ....              [] ->o/p
+[i/p]->[]  [] ....
+
+*/
+
+class SimpleNeuralNetwork
+{
+
+};
